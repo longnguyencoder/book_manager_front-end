3ce5299dbf12480e7a20e3d2f3b2d78fa476713f
longnguyencoder <long0398481719@gmail.com>
2025-03-14 13:36:18 +0700

    create footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.css';
 import Navbar from './layouts/Navbar';
+import Footer from './layouts/Footer';
 
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Footer />
     </div>
   );
 }
